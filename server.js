ea5ce7cc81a6dfd1e2b4877dbac8bf434093caac
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-04 12:51:40 -0600

    Pass first test for showing all ideas

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Mod4 Final';
 
 app.get('/', (request, response) => {
   response.send('It isssss a final!!');
+});
+
+app.get('/api/v1/ideas', (request, response) => {
+  database('ideas').select()
+    .then((ideas) => {
+      response.status(200).json(ideas);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
 });
 
 app.listen(app.get('port'), () => {
